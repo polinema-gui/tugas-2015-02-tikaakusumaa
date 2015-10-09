4187e80da0045a20cd5a3b480e7373ee9a51fe01
tikaakusumaa <tikakusuma03@gmail.com>
2015-10-09 17:42:00 +0700

    Tugas Kalkulator
    Nama : Tika Kusuma Wardhani
    Kelas : 2 MI E
    Absen : 28
    NIM : 1431140024
    maaf pak terlambat ..

---
+++ Tugas Kalkulator/Form1.Designer.vb
@@ -0,0 +1,222 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.satu = New System.Windows.Forms.Button()
+        Me.dua = New System.Windows.Forms.Button()
+        Me.tiga = New System.Windows.Forms.Button()
+        Me.empat = New System.Windows.Forms.Button()
+        Me.lima = New System.Windows.Forms.Button()
+        Me.enam = New System.Windows.Forms.Button()
+        Me.tujuh = New System.Windows.Forms.Button()
+        Me.delapan = New System.Windows.Forms.Button()
+        Me.sembilan = New System.Windows.Forms.Button()
+        Me.clear = New System.Windows.Forms.Button()
+        Me.kurang = New System.Windows.Forms.Button()
+        Me.nol = New System.Windows.Forms.Button()
+        Me.hasil = New System.Windows.Forms.Button()
+        Me.tambah = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 12)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.TextBox1.Size = New System.Drawing.Size(243, 45)
+        Me.TextBox1.TabIndex = 0
+        '
+        'satu
+        '
+        Me.satu.Location = New System.Drawing.Point(16, 162)
+        Me.satu.Name = "satu"
+        Me.satu.Size = New System.Drawing.Size(44, 42)
+        Me.satu.TabIndex = 1
+        Me.satu.Text = "1"
+        Me.satu.UseVisualStyleBackColor = True
+        '
+        'dua
+        '
+        Me.dua.Location = New System.Drawing.Point(64, 162)
+        Me.dua.Name = "dua"
+        Me.dua.Size = New System.Drawing.Size(48, 42)
+        Me.dua.TabIndex = 2
+        Me.dua.Text = "2"
+        Me.dua.UseVisualStyleBackColor = True
+        '
+        'tiga
+        '
+        Me.tiga.Location = New System.Drawing.Point(118, 162)
+        Me.tiga.Name = "tiga"
+        Me.tiga.Size = New System.Drawing.Size(46, 42)
+        Me.tiga.TabIndex = 3
+        Me.tiga.Text = "3"
+        Me.tiga.UseVisualStyleBackColor = True
+        '
+        'empat
+        '
+        Me.empat.Location = New System.Drawing.Point(16, 114)
+        Me.empat.Name = "empat"
+        Me.empat.Size = New System.Drawing.Size(44, 42)
+        Me.empat.TabIndex = 4
+        Me.empat.Text = "4"
+        Me.empat.UseVisualStyleBackColor = True
+        '
+        'lima
+        '
+        Me.lima.Location = New System.Drawing.Point(64, 114)
+        Me.lima.Name = "lima"
+        Me.lima.Size = New System.Drawing.Size(48, 42)
+        Me.lima.TabIndex = 5
+        Me.lima.Text = "5"
+        Me.lima.UseVisualStyleBackColor = True
+        '
+        'enam
+        '
+        Me.enam.Location = New System.Drawing.Point(118, 114)
+        Me.enam.Name = "enam"
+        Me.enam.Size = New System.Drawing.Size(46, 42)
+        Me.enam.TabIndex = 6
+        Me.enam.Text = "6"
+        Me.enam.UseVisualStyleBackColor = True
+        '
+        'tujuh
+        '
+        Me.tujuh.Location = New System.Drawing.Point(16, 63)
+        Me.tujuh.Name = "tujuh"
+        Me.tujuh.Size = New System.Drawing.Size(46, 45)
+        Me.tujuh.TabIndex = 7
+        Me.tujuh.Text = "7"
+        Me.tujuh.UseVisualStyleBackColor = True
+        '
+        'delapan
+        '
+        Me.delapan.Location = New System.Drawing.Point(64, 63)
+        Me.delapan.Name = "delapan"
+        Me.delapan.Size = New System.Drawing.Size(46, 45)
+        Me.delapan.TabIndex = 8
+        Me.delapan.Text = "8"
+        Me.delapan.UseVisualStyleBackColor = True
+        '
+        'sembilan
+        '
+        Me.sembilan.Location = New System.Drawing.Point(116, 63)
+        Me.sembilan.Name = "sembilan"
+        Me.sembilan.Size = New System.Drawing.Size(48, 45)
+        Me.sembilan.TabIndex = 9
+        Me.sembilan.Text = "9"
+        Me.sembilan.UseVisualStyleBackColor = True
+        '
+        'clear
+        '
+        Me.clear.Location = New System.Drawing.Point(180, 63)
+        Me.clear.Name = "clear"
+        Me.clear.Size = New System.Drawing.Size(75, 97)
+        Me.clear.TabIndex = 10
+        Me.clear.Text = "Clear"
+        Me.clear.UseVisualStyleBackColor = True
+        '
+        'kurang
+        '
+        Me.kurang.Location = New System.Drawing.Point(180, 166)
+        Me.kurang.Name = "kurang"
+        Me.kurang.Size = New System.Drawing.Size(75, 38)
+        Me.kurang.TabIndex = 11
+        Me.kurang.Text = "-"
+        Me.kurang.UseVisualStyleBackColor = True
+        '
+        'nol
+        '
+        Me.nol.Location = New System.Drawing.Point(12, 210)
+        Me.nol.Name = "nol"
+        Me.nol.Size = New System.Drawing.Size(48, 39)
+        Me.nol.TabIndex = 12
+        Me.nol.Text = "0"
+        Me.nol.UseVisualStyleBackColor = True
+        '
+        'hasil
+        '
+        Me.hasil.Location = New System.Drawing.Point(66, 210)
+        Me.hasil.Name = "hasil"
+        Me.hasil.Size = New System.Drawing.Size(98, 39)
+        Me.hasil.TabIndex = 13
+        Me.hasil.Text = "="
+        Me.hasil.UseVisualStyleBackColor = True
+        '
+        'tambah
+        '
+        Me.tambah.Location = New System.Drawing.Point(180, 210)
+        Me.tambah.Name = "tambah"
+        Me.tambah.Size = New System.Drawing.Size(75, 39)
+        Me.tambah.TabIndex = 14
+        Me.tambah.Text = "+"
+        Me.tambah.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.tambah)
+        Me.Controls.Add(Me.hasil)
+        Me.Controls.Add(Me.nol)
+        Me.Controls.Add(Me.kurang)
+        Me.Controls.Add(Me.clear)
+        Me.Controls.Add(Me.sembilan)
+        Me.Controls.Add(Me.delapan)
+        Me.Controls.Add(Me.tujuh)
+        Me.Controls.Add(Me.enam)
+        Me.Controls.Add(Me.lima)
+        Me.Controls.Add(Me.empat)
+        Me.Controls.Add(Me.tiga)
+        Me.Controls.Add(Me.dua)
+        Me.Controls.Add(Me.satu)
+        Me.Controls.Add(Me.TextBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents satu As System.Windows.Forms.Button
+    Friend WithEvents dua As System.Windows.Forms.Button
+    Friend WithEvents tiga As System.Windows.Forms.Button
+    Friend WithEvents empat As System.Windows.Forms.Button
+    Friend WithEvents lima As System.Windows.Forms.Button
+    Friend WithEvents enam As System.Windows.Forms.Button
+    Friend WithEvents tujuh As System.Windows.Forms.Button
+    Friend WithEvents delapan As System.Windows.Forms.Button
+    Friend WithEvents sembilan As System.Windows.Forms.Button
+    Friend WithEvents clear As System.Windows.Forms.Button
+    Friend WithEvents kurang As System.Windows.Forms.Button
+    Friend WithEvents nol As System.Windows.Forms.Button
+    Friend WithEvents hasil As System.Windows.Forms.Button
+    Friend WithEvents tambah As System.Windows.Forms.Button
+
+End Class
---
+++ Tugas Kalkulator/Form1.vb
@@ -0,0 +1,137 @@
+﻿Public Class Form1
+    Dim a As String
+    Dim samadengan As String
+    Dim inputSatu As Integer
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        TextBox1.Text = "0"
+    End Sub
+
+    Private Sub nol_Click(sender As Object, e As EventArgs) Handles nol.Click
+        If (a = "0") Then
+            a = "0"
+        Else
+            a &= "0"
+        End If
+        TextBox1.Text = a
+    End Sub
+
+    Private Sub satu_Click(sender As Object, e As EventArgs) Handles satu.Click
+        If (a = "0") Then
+            a = "1"
+        Else
+            a &= "1"
+        End If
+        TextBox1.Text = a
+
+    End Sub
+
+    Private Sub dua_Click(sender As Object, e As EventArgs) Handles dua.Click
+        If (a = "0") Then
+            a = "2"
+        Else
+            a &= "2"
+        End If
+        TextBox1.Text = a
+    End Sub
+
+    Private Sub tiga_Click(sender As Object, e As EventArgs) Handles tiga.Click
+        If (a = "0") Then
+            a = "3"
+        Else
+            a &= "3"
+        End If
+        TextBox1.Text = a
+    End Sub
+
+
+    Private Sub empat_Click(sender As Object, e As EventArgs) Handles empat.Click
+        If (a = "0") Then
+            a = "4"
+        Else
+            a &= "4"
+        End If
+        TextBox1.Text = a
+    End Sub
+
+
+    Private Sub lima_Click(sender As Object, e As EventArgs) Handles lima.Click
+        If (a = "0") Then
+            a = "5"
+        Else
+            a &= "5"
+        End If
+        TextBox1.Text = a
+    End Sub
+
+
+    Private Sub enam_Click(sender As Object, e As EventArgs) Handles enam.Click
+        If (a = "0") Then
+            a = "6"
+        Else
+            a &= "6"
+        End If
+        TextBox1.Text = a
+    End Sub
+
+
+    Private Sub tujuh_Click(sender As Object, e As EventArgs) Handles tujuh.Click
+        If (a = "0") Then
+            a = "7"
+        Else
+            a &= "7"
+        End If
+        TextBox1.Text = a
+    End Sub
+
+
+    Private Sub delapan_Click(sender As Object, e As EventArgs) Handles delapan.Click
+        If (a = "0") Then
+            a = "8"
+        Else
+            a &= "8"
+        End If
+        TextBox1.Text = a
+    End Sub
+
+
+    Private Sub sembilan_Click(sender As Object, e As EventArgs) Handles sembilan.Click
+        If (a = "0") Then
+            a = "9"
+        Else
+            a &= "9"
+        End If
+        TextBox1.Text = a
+    End Sub
+
+
+
+    Private Sub clear_Click(sender As Object, e As EventArgs) Handles clear.Click
+        TextBox1.Text = "0"
+        a = Nothing
+    End Sub
+
+
+    Private Sub hasil_Click(sender As Object, e As EventArgs) Handles hasil.Click
+        Select Case Me.samadengan
+            Case "c"
+                TextBox1.Text = Convert.ToString(inputSatu + TextBox1.Text)
+            Case "d"
+                TextBox1.Text = Convert.ToString(inputSatu + TextBox1.Text)
+        End Select
+    End Sub
+
+
+    Private Sub tambah_Click(sender As Object, e As EventArgs) Handles tambah.Click
+        Me.samadengan = "c"
+        Me.inputSatu = TextBox1.Text
+        a = Nothing
+    End Sub
+
+
+    Private Sub kurang_Click(sender As Object, e As EventArgs) Handles kurang.Click
+        Me.samadengan = "d"
+        Me.inputSatu = TextBox1.Text
+        a = Nothing
+    End Sub
+End Class
+ 
